2d06a56ab215fb741586d8b2e3169e622c1daba2
Abbascode <abbasyim1@gmail.com>
2024-11-04 12:04:02 +0000

    little changes

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// let loginForm = document.querySelector(".my-form");
-
-// loginForm.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   let email = document.getElementById("email");
-//   let password = document.getElementById("password");
-
-//   console.log("Email:", email.value);
-//   console.log("Password:", password.value);
-//   // process and send to API
-// });
-
 const form = document.getElementById('myForm');
 
 form.addEventListener('submit', (e) => {
